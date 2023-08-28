97b6266624dc6a333c2441136e74fb6d3ad793c5
Alemayehu <alluwa2127@gmail.com>
2023-08-28 22:31:03 +0300

    protect a resume route

--- Backend/routes/resumeRoute.js
+++ Backend/routes/resumeRoute.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { resumeController } = require('../controllers/resumeController')
-router.post('/resume', resumeController);
+const {authorizatin} = require('../middlewares/auth');
+const seeker = authorizatin(["jobSeeker"]);
+router.post('/resume',seeker, resumeController);
 module.exports = router;
